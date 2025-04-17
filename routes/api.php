ade25f8063526583890f5ee0488c072de168cb46
belm960 <samuelbefikadu10@gmail.com>
2025-04-17 11:51:28 +0300

    Added the landing page content adding feature

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\VehicleCategoryController;
 use App\Http\Controllers\SSOController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\PasswordResetController;
+use App\Http\Controllers\LandingContentController;
 use App\Http\Controllers\ReviewController;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Mail;
@@ -31,6 +32,24 @@ Route::get('/reviews', [ReviewController::class, 'index']);
 //     return 'Test Gmail sent!';
 // });
 
+// General Info
+Route::post('/general-info', [LandingContentController::class, 'setGeneralInfo']);
+Route::put('/general-info/{key}', [LandingContentController::class, 'updateGeneralInfo']);
+Route::get('/general-info', [LandingContentController::class, 'listGeneralInfo']);
+Route::get('/general-info/{key}', [LandingContentController::class, 'getGeneralInfo']);
+Route::delete('/general-info/{key}', [LandingContentController::class, 'deleteGeneralInfo']);
+
+// FAQ
+Route::get('/faqs', [LandingContentController::class, 'listFaqs']);
+Route::post('/faqs', [LandingContentController::class, 'addFaq']);
+Route::put('/faqs/{id}', [LandingContentController::class, 'updateFaq']);
+Route::delete('/faqs/{id}', [LandingContentController::class, 'deleteFaq']);
+
+// Landing Sections
+Route::get('/sections', [LandingContentController::class, 'listSections']);
+Route::post('/sections', [LandingContentController::class, 'addSection']);
+Route::put('/sections/{id}', [LandingContentController::class, 'updateSection']);
+Route::delete('/sections/{id}', [LandingContentController::class, 'deleteSection']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/update-password', [AuthController::class, 'updatePassword']);
