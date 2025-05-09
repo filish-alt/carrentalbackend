4c721b629ae446da62f1aafb1a018c1c6990e385
filagot <filagotshewafera@gmail.com>
2025-05-09 09:46:13 +0300

    car image

--- app/Models/CarImage.php
+++ app/Models/CarImage.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+use Illuminate\Support\Facades\Storage;
+
 class CarImage extends Model
 {
 
@@ -15,11 +17,16 @@ protected $fillable = [
     'image_path',
  ];
 
-
+ protected $appends = ['image_url'];
 
  public function car() 
   {
     return $this->belongsTo(Car::class);
+  }
+
+  public function getImageUrlAttribute()
+  {
+      return Storage::url($this->image_path);
   }
 
 }
