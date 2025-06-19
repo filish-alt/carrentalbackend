0fb1b86b7a80ffa69ab4a1835cee7bf48959b190
filagot <filagotshewafera@gmail.com>
2025-06-19 13:00:13 +0300

    change

--- app/Http/Controllers/HomereviewController.php
+++ app/Http/Controllers/HomereviewController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Homereview;
-use App\Models\Home;
+use App\Models\Homes;
 
 
 class HomereviewController extends Controller
@@ -35,7 +35,7 @@ public function index()
 }
 
 //get review for specific home
-public function reviewsForHome(Home $home)
+public function reviewsForHome(Homes $home)
 {
     $reviews = Homereview::where('home_id', $home->id)
         ->with(['user', 'home'])
